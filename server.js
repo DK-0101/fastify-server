335a88608e77e91c394a5ae0183efeb6a48a8563
DK-01-01 <dayksontavares23@gmail.com>
2025-07-28 18:03:04 -0300

    API node using festify

---
+++ server.js
@@ -0,0 +1,54 @@
+import { fastify } from "fastify";
+import { DatabasePostgres } from "./database-postgres.js";
+
+const server = fastify()
+
+
+const database = new DatabasePostgres()
+
+server.post('/videos', async (req, replay) => {
+    const { title, description, duration } = req.body
+
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return replay.status(201).send()
+})
+
+server.get('/videos', async (req) => {
+    const search = req.query.search
+
+    console.log(search)
+
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (req, replay) => {
+    const videoId = req.params.id
+    const { title, description, duration } = req.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return replay.status(204).send()
+})
+
+server.delete('/videos/:id', async (req, replay) => {
+    const videoId = req.params.id
+
+    await database.delete(videoId)
+
+    return replay.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 9090,
+})
